e8d077579a2614762c63579f048135de5a86f556
shreyasgadave <shreyasgadave777@gmail.com>
2025-03-30 19:29:24 +0530

    vercel File 4

--- Backend/index.js
+++ Backend/index.js
@@ -19,12 +19,12 @@ app.use(express.json());
 
 ConnectDB(MONGO_URI);
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*"); // Allow all origins
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*"); // Allow all origins
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
  
 app.use(LoginRouter)
 
