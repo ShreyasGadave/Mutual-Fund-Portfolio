e551a88618767708509bc86c9384c11e33514086
shreyasgadave <shreyasgadave777@gmail.com>
2025-03-30 19:27:02 +0530

    vercel File 3

--- Backend/index.js
+++ Backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 3009;
 const MONGO_URI = process.env.MONGODB_URI;
 
-app.use(cors({ origin: process.env.FRONTEND_URI || "*", CredentialCloudinary: true }));
+app.use(cors());
 app.use(express.json());
 
 ConnectDB(MONGO_URI);
