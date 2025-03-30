80053bb28697ef904b26ec9330f14d5a104a77ad
shreyasgadave <shreyasgadave777@gmail.com>
2025-03-30 16:30:37 +0530

    changes

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,7 @@ const LoginRouter = require("./Controller/LoginRouter");
 
 const app = express();
 const PORT = process.env.PORT || 3009;
-const MONGO_URI = process.env.MONGO_URI/AniketMangave;
+const MONGO_URI = process.env.MONGO_URI;
 
 app.use(cors({ origin: process.env.FRONTEND_URI || "*", CredentialCloudinary: true }));
 app.use(express.json());
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
-
+ 
 app.use(LoginRouter)
 
 app.use("/admin",AdminRouter);
