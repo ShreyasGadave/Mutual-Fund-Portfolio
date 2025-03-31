b7c89927dcc10c49d65765a628cc8f2e1345e7d2
shreyasgadave <shreyasgadave777@gmail.com>
2025-03-31 17:26:05 +0530

    Navigation /admin resloved

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
       // console.log("User is logged in:", user.email);
       
       if (!user) {
-        navigate("/admin");
+        navigate("/");
       }
     });
   }, [navigate]);
