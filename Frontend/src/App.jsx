8d9477f6af0ba6ca12133f07880b8ff0e960867b
shreyasgadave <shreyasgadave777@gmail.com>
2025-07-16 23:10:59 +0530

    Admin Redirect

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,11 @@
-import React, { useEffect } from "react";
-import { Route, Routes, useNavigate, useLocation } from "react-router-dom";
+import React, { useEffect ,useState} from "react";
+import {
+  Route,
+  Routes,
+  useNavigate,
+  useLocation,
+  Navigate,
+} from "react-router-dom";
 import Home from "./Pages/Home";
 import Service from "./Pages/Service";
 import AdminServices from "./Pages/AdminServices";
@@ -10,31 +16,45 @@ import AdminInfo from "./Pages/AdminProfile";
 import About from "./Pages/About";
 import { auth } from "./Services/Firebase";
 import { onAuthStateChanged } from "firebase/auth";
-import ProtectedRoute from "./Components/ProtectedRoute"; 
+import ProtectedRoute from "./Components/ProtectedRoute";
 import Login from "./Pages/Login";
 
 const App = () => {
   const navigate = useNavigate();
   const location = useLocation();
-  
+  const [user, setuser] = useState();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      setuser(user);
+    });
+  });
+
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       // Only redirect to login if the user is trying to access a protected route
-      const protectedRoutes = ["/admin/profile", "/admin/about", "/admin/service", "/admin/testimonials"];
-      
+      const protectedRoutes = [
+        "/admin/profile",
+        "/admin/about",
+        "/admin/service",
+        "/admin/testimonials",
+      ];
+
       if (!user && protectedRoutes.includes(window.location.pathname)) {
         navigate("/login");
       }
     });
   }, [navigate]);
-  
 
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/service/:id" element={<Service />} />
-        <Route path="/login" element={<Login />} />
+        <Route
+          path="/login"
+          element={user ? <Navigate to="/admin/profile" /> : <Login />}
+        />
         <Route path="/about" element={<About />} />
 
         {/* 🛑 Protected Routes Start Here */}
